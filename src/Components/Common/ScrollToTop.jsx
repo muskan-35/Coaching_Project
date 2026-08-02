a33305391b918d36f417537c4d9594857e22726e
MuskanChaurasiya <chaurasiyamushkan3@gmail.com>
2026-08-02 22:16:58 +0530

    Refactor ScrollToTop to use useLayoutEffect
    Replaced useEffect with useLayoutEffect for scroll restoration behavior.

--- src/Components/Common/ScrollToTop.jsx
+++ src/Components/Common/ScrollToTop.jsx
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-const ScrollToTop = () => {
+function ScrollToTop() {
   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
+  useLayoutEffect(() => {
+    if ("scrollRestoration" in window.history) {
+      window.history.scrollRestoration = "manual";
+    }
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
   }, [pathname]);
 
   return null;
-};
+}
 
 export default ScrollToTop;
